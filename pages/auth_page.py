f3733f8bef5195cbd770ce7fc647494973590ed7
Savchuk-PA <savchukpa@icloud.com>
2022-11-21 13:27:23 +0300

    added random str-num generator for short pass

--- pages/auth_page.py
+++ pages/auth_page.py
@@ -3,6 +3,7 @@ import time
 from data.data import Auth
 from locators.auth_page_locators import AuthPageLocators
 from pages.base_page import BasePage
+import selenium
 
 
 class AuthPage(BasePage):
@@ -13,3 +14,12 @@ class AuthPage(BasePage):
         self.element_is_visible(self.locators.INPUT_PHONE_OR_EMAIL_BTN).send_keys(login)
         self.element_is_visible(self.locators.INPUT_PASSWORD_BTN).send_keys(password)
         self.element_is_clickable(self.locators.SIGN_IN_BTN).click()
+        time.sleep(1)
+
+    def get_error_login(self, log_or_phone: str):
+        """Возвращает текст ошибки. В качестве параметра нужно перадать login,
+        вернет ошибку логина, елси передать password, вернет ошибку пароля"""
+        if log_or_phone == 'login':
+            return self.elements_are_visible(self.locators.AUTH_ERROR_MESSAGE_LOGIN)[0].get_attribute("data-error")
+        if log_or_phone == 'password':
+            return self.elements_are_visible(self.locators.AUTH_ERROR_MESSAGE_LOGIN)[1].get_attribute("data-error")
